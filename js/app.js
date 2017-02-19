9e3b2bd316e357e48c600f5236718d5af5a51f44
[heyoni] <[jrevah@gmail.com]>
2017-02-19 16:51:19 -0500

    removed unused code

--- js/app.js
+++ js/app.js
@@ -1,22 +1,104 @@
-var Model = [];
+var radius;
 
-var View = function () {
-
-}
-
-var ViewModel = function () {
+var YelpViewModel = function () {
     var self = this;
-    this.lat = ko.observable(map.getCenter().lat());
-    this.lng = ko.observable();
 
-    this.setLat = function() {
-        self.lat(map.getCenter().lat());
-    }
+    self.lat = ko.observable(map.getCenter().lat());
+    self.lng = ko.observable(map.getCenter().lat());
+
+    // queryYelp(self.lat(), self.lng());
+    // fetchToken();
+    calculateRadius();
+    searchYelp(self.lat(), self.lng());
+}
+
+setMarkers = function (yelpResults) {
+    ydata = yelpResults;
+    yelpResults.forEach(function (result) {
+        lat = result.location.coordinate.latitude;
+        lng = result.location.coordinate.longitude;
+        var marker = new google.maps.Marker({
+            position: {lat: lat, lng: lng},
+            map: map
+        });
+        console.log(result)
+    })
+}
+calculateRadius = function () {
+    var bounds = map.getBounds();
+    var boundsLeft = new google.maps.LatLng(bounds.f.f, bounds.b.f);
+    var boundsRight = new google.maps.LatLng(bounds.f.b, bounds.b.b);
+    radius = google.maps.geometry.spherical.computeDistanceBetween(boundsRight, boundsLeft) / 2;
+}
+
+clearMarkers = function () {
 
 }
 
-var NYTViewModel = function() {
-    var nytKey = '5579364465da4d33bdb204c6b31c32cb';
+function cb(data) {
+    console.log("cb: " + JSON.stringify(data));
 }
 
-ko.applyBindings(new ViewModel());
+function searchYelp(latitude, longitude) {
+    var auth = {
+        //
+        // Update with your auth tokens.
+        //
+        consumerKey: "4FsxJvMCGNmATdc58K6v-g",
+        consumerSecret: "uppb9ixG1rTzaVj63kSugCxcpMg",
+        accessToken: "tXNGmc-C_jdqQvhEPXSh84iMbZb7bj0n",
+        // This example is a proof of concept, for how to use the Yelp v2 API with javascript.
+        // You wouldn't actually want to expose your access token secret like this in a real application.
+        accessTokenSecret: "MxMfBMI5VVwEcsLTAQw7blLU-Qw",
+        serviceProvider: {
+            signatureMethod: "HMAC-SHA1"
+        }
+    };
+
+    var terms = 'food';
+    var near = 'Brooklyn';
+
+    var accessor = {
+        consumerSecret: auth.consumerSecret,
+        tokenSecret: auth.accessTokenSecret
+    };
+
+    var parameters = [];
+    parameters.push(['term', terms]);
+    parameters.push(['location', near]);
+    parameters.push(['cll', latitude + "," + longitude]);
+    parameters.push(['radius_filter', radius]);
+    parameters.push(['callback', 'cb']);
+    parameters.push(['oauth_consumer_key', auth.consumerKey]);
+    parameters.push(['oauth_consumer_secret', auth.consumerSecret]);
+    parameters.push(['oauth_token', auth.accessToken]);
+    parameters.push(['oauth_signature_method', 'HMAC-SHA1']);
+
+    var message = {
+        'action': 'https://api.yelp.com/v2/search',
+        'method': 'GET',
+        'parameters': parameters
+    };
+
+    OAuth.setTimestampAndNonce(message);
+    OAuth.SignatureMethod.sign(message, accessor);
+
+    var parameterMap = OAuth.getParameterMap(message.parameters);
+
+    $.ajax({
+        'url': message.action,
+        'data': parameterMap,
+        'dataType': 'jsonp',
+        'cache': true,
+        'success': function (data) {
+            setMarkers(data.businesses);
+            console.log('success');
+        }
+    })
+        .fail(function (jqXHR, textStatus, errorThrown) {
+                console.log('error[' + errorThrown + '], status[' + textStatus + '], jqXHR[' + JSON.stringify(jqXHR) + ']');
+            }
+        );
+}
+
+ko.applyBindings(new YelpViewModel());
--- js/maps.js
+++ js/maps.js
@@ -1,40 +1,15 @@
 var map;
+
+
 function initMap() {
     defaultPos = {lat: 40.695003, lng: -73.965920};
     map = new google.maps.Map(document.getElementById('map'), {
         center: defaultPos,
         zoom: 15
     });
-    var marker = new google.maps.Marker({
-        position: defaultPos,
-        map: map
-    });
-
-    var request = {
-        location: defaultPos,
-        radius: 500,
-        types: ['store']
-    };
-
-    service = new google.maps.places.PlacesService(map);
-    service.nearbySearch(request, callback);
 }
 
-
-function callback(results, status) {
-    if (status == google.maps.places.PlacesServiceStatus.OK) {
-        results.forEach(function (result) {
-            var place = result;
-            var lat = place.geometry.location.lat()
-            var lng = place.geometry.location.lng()
-            newLoc = {
-                position: {
-                    lat: lat,
-                    lng: lng
-                },
-                map: map
-            }
-            new google.maps.Marker(newLoc, map);
-        })
-    }
-}
+//
+// map.addListener('bounds_changed', function () {
+//     ;
+// })
