0fb2afbeff05620f936d225572c979e45d4af788
yoni <jrevah@gmail.com>
2017-03-04 23:35:11 -0500

    Added some responsive elements. Changed the layout to use awesome-font logos

--- js/app.js
+++ js/app.js
@@ -1,8 +1,4 @@
 var radius;
-var gData;
-var gYata
-var listMarker;
-var markerMarker;
 
 var YelpViewModel = function () {
     var self = this;
@@ -16,32 +12,45 @@ var YelpViewModel = function () {
     self.compResults = ko.computed(function () {
         self.filteredResults.removeAll();
         // iterates through our results array
-        self.results().find(function(result) {
+        self.results().find(function (result) {
             // performs a regex on the name string with the value in the filter input box
             var re = new RegExp(self.filter() || '' + '.*?', 'i');
-            var match = result[0].name.match(re);
-            if ( match != null) {
+            var match = result.restaurant.name.match(re);
+            if (match != null) {
                 // repopulates the filteredResults with any item that matched our filter
                 self.filteredResults.push(result);
             }
         });
-        return(self.filteredResults());
+        return (self.filteredResults());
     });
 
-    self.populateResults = function (data) {
+    self.populateResults = function () {
         [self.message, self.parameterMap] = searchYelp();
         // clear out all the results
-        self.results.removeAll();
+        // self.results.removeAll();
+        self.removeEntry();
         // query Yelp with new geographical location if it applies
         // results will be added to self.results() if successful
         self.query(self.message, self.parameterMap);
     };
 
-    self.remove = function (business) {
-        business[1].setMap(null);
-        self.results.remove(business);
-
-        console.log(business[0].name);
+    self.removeEntry = function (business) {
+        if (business == null) {
+            self.results().forEach(function (restaurant) {
+                // if you remove an object while iterating through it, the item in the position ahead will move down,
+                // but the loop will move up in the index, leaving behind half of the results unprocessed.
+                // somehow destroy() works because it hides the results rather than delete them.
+                self.results.destroy(restaurant);
+                delete restaurant.infowindow
+                restaurant.marker.setMap(null);
+                // restaurant.marker.setMap(null);
+            })
+        } else {
+            console.log('business is not null');
+            self.results.destroy(business);
+            delete business.infowindow;
+            business.marker.setMap(null);
+        }
     };
 
     self.query = function () {
@@ -53,35 +62,35 @@ var YelpViewModel = function () {
             'success': function (data) {
                 console.log('successfully fetched Yelp data');
                 data.businesses.forEach(function (business) {
-                    self.results.push([business, setMarker(business), setInfoWindow(business)]);
-                    modal = markerModel(business);
+                    // console.log(markerModel(business));
+                    self.results.push(markerModel(business));
                 })
             },
+            'error': function (jqXHR, textStatus, errorThrown) {
+                self.results.push(markerModel(textStatus));
+                // self.results.push([{name: 'Error loading results (' + textStatus + ')'}, 0]);
+                console.log('error[' + errorThrown + '], status[' + textStatus + '], jqXHR[' + JSON.stringify(jqXHR) + ']');
+            },
             'timeout': 8000,
-            'statusCode': {
-                400: function () {
-                    console.log('error 400. query error.');
-                }
-            }
         })
-            .fail(function (jqXHR, textStatus, errorThrown) {
-                    self.results.push([{name: 'Error loading results (' + textStatus + ')'}, 0]);
-                    console.log('error[' + errorThrown + '], status[' + textStatus + '], jqXHR[' + JSON.stringify(jqXHR) + ']');
-                }
-            );
     }
 };
-//implement this model for map markers and decouple this logic from viewModel
+// implement this model for map markers and decouple this logic from viewModel
 var markerModel = function (result) {
-    this.restaurant = {restaurant: result};
-    this.restaurant.infowindow = setInfoWindow(result);
-    this.restaurant.marker = setMarker(result, this.restaurant.infowindow);
-    console.log(this.restaurant);
+    // console.log(typeof(result) == 'object');
+    // check if result is a restaurant object, else assume it's an error code
+    if (typeof(result) == 'object') {
+        this.restaurant = {restaurant: result};
+        this.restaurant.infowindow = setInfoWindow(result);
+        this.restaurant.marker = setMarker(result, this.restaurant.infowindow);
+    } else {
+        this.restaurant = {restaurant: {name: 'error: ' + result}};
+        return this.restaurant
+    }
     return this.restaurant;
 };
 setInfoWindow = function (result) {
     var contentStr = result.name + '<br>' + result.phone + '<br>' + 'result.rating' + '<br>' + result.review_count;
-    console.log(contentStr);
     return new google.maps.InfoWindow({
         content: contentStr
     })
@@ -107,16 +116,11 @@ popupInfo = function (marker, infowindow) {
     setTimeout(function () {
         marker.setAnimation(null);
     }, 2000);
+    // close any opened infowindow
+    viewModel.results().forEach(function (restaurant) {
+        restaurant.infowindow.close();
+    })
     infowindow.open(map, marker);
-    //
-    // console.log(marker);
-    // markerMarker = marker;
-    // viewModel.results().find(function (subarray) {
-    //     if (subarray.indexOf(markerMarker) == 1) {
-    //         console.log(subarray[0].name);
-    //     }
-    // })
-    console.log('marker clicked...');
 };
 calculateRadius = function () {
     var bounds = map.getBounds();
@@ -126,29 +130,41 @@ calculateRadius = function () {
     radius = google.maps.geometry.spherical.computeDistanceBetween(southWest, northEast) / 2;
 };
 fitToRadius = function (radiusLimit) {
-    // lat/lng per 1 meter
+    // zoom in until we're within our prescribed radius
     while (radius >= radiusLimit) {
         calculateRadius();
-        // latMeter = Math.abs((northEast.lat() - map.getCenter().lat()) / radius);
-        // lngMeter = Math.abs((northEast.lng() - map.getCenter().lng()) / radius);
-        // northBoundary = map.getCenter().lat() + (latMeter * 39000);
-        // southBoundary = map.getCenter().lat() - (latMeter * 39000);
-        // westBoundary = map.getCenter().lng() - (lngMeter * 39000);
-        // eastBoundary = map.getCenter().lng() + (lngMeter * 39000);
-        // // this function only makes sure to contain the boundaries and may be larger than what we need
-        // map.fitBounds({south: southBoundary, west: westBoundary, north: northBoundary, east: eastBoundary});
         map.setZoom(map.getZoom() + 1);
     }
-}
-clearMarkers = function () {
-
 };
 
+var sideBar = {
+    hidden: 0,
+    offset: 0,
+    toggleSidebar : function () {
+        if (this.hidden == 0) {
+            $('div.results').css('right', -this.offset);
+            $('a.results-hide').html('<i class="fa fa-chevron-left fa-lg" aria-hidden="true">');
+            this.hidden = 1;
+        } else {
+            this.hidden = 0;
+            $('div.results').css('right', 0);
+            $('a.results-hide').html('<i class="fa fa-chevron-right fa-lg" aria-hidden="true">');
+        }
+    },
+    setOffset: function () {
+        console.log('running sidebardotoffset');
+        this.offset = ($('div.results').outerWidth(true) - $('div.results div.results-list').outerWidth(true)) / 2 + $('div.results div.results-list').outerWidth(true);
+    }
+};
+$(".results").on("DOMSubtreeModified", function () {
+    sideBar.setOffset();
+});
+// $(".results").on("DOMSubtreeModified.sideBar", sideBar.setOffset);
 function cb() {
     // console.log("cb: " + JSON.stringify(data));
     console.log('success');
 
-}
+};
 
 function searchYelp() {
     calculateRadius();
@@ -199,17 +215,16 @@ function searchYelp() {
 
     var parameterMap = OAuth.getParameterMap(message.parameters);
     return [message.action, parameterMap];
-}
+};
 var viewModel = new YelpViewModel();
 ko.applyBindings(viewModel);
 
-function getMethods(obj)
-{
+function getMethods(obj) {
     var res = [];
-    for(var m in obj) {
-        if(typeof obj[m] == "function") {
+    for (var m in obj) {
+        if (typeof obj[m] == "function") {
             res.push(m)
         }
     }
     return res;
-}
+};
